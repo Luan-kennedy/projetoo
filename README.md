fe47f4613eb663e45161159df5dd262cc57afb80
Luan-kennedy <luan.kennedy.silva.ls@gmail.com>
2024-11-26 16:58:54 -0300

    documentaçao
    documentaçao do codigo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Sistema de Agendamentos.
-    Exibe os Agendamentos
+    Exibe os Agendamentos.
 ![Página de Agendamentos.](./prints/agendamento.png)
-    Necessario preencher todos os campos, mesmo que nao haja alteração
+    Necessario preencher todos os campos, mesmo que nao haja alteração.
 ![Página de Atualizaçao.](./prints/atualiz.png)
  
--- protect.php
+++ protect.php
@@ -2,7 +2,6 @@
 
 session_start();
 
-
 if(!isset($_SESSION["id"])){ // verifica se estite login de acordo com o nivel de acesso, caso nao esteja logado mata o script e apresenta o caminho para logar
     die("<h2>Você não pode acessar esta Pagina porque não esta logado.</h2>
     <h3><p><a href=\"login.php\"><i class=\"bi bi-box-arrow-in-right\"></i> Login</a></p></h3>"); 
